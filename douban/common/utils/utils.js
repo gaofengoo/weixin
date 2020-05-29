adc7c203a5f57474f7a3a9a35ea79137a9c2c7fe
gaofengoo <15879831437@163.com>
2020-05-29 18:43:21 +0800

    更新

--- douban/common/utils/utils.js
+++ douban/common/utils/utils.js
@@ -1,16 +1,16 @@
 var handle;
 
 handle = {
-  showLoading : function () {
-    wx.showToast( {
-      title : '数据加载中',
-      icon : 'loading',
-      duration : 10000
-    } );
-  },
-  hideLoading : function() {
-    wx.hideToast();
-  }
+	showLoading : function () {
+		wx.showToast( {
+			title : '数据加载中',
+			icon : 'loading',
+			duration : 10000
+		} );
+	},
+	hideLoading : function() {
+		wx.hideToast();
+	}
 }
 
 module.exports = handle;
